2e8096c72e597da662eb5db8e32737993a776298
张海波 <zhb38469935@hotmail.com>
2022-10-27 10:17:21 +0800

    update templates

--- Templates/WHSPlugin6/Template/Template.Device.Alias/Resources/Strings.Designer.cs
+++ Templates/WHSPlugin6/Template/Template.Device.Alias/Resources/Strings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WHSPlugin5.Template.Template.Device.Alias.Resources {
+namespace WHSPlugin6.Template.Template.Device.Alias.Resources {
     using System;
     
     
@@ -19,7 +19,7 @@ namespace WHSPlugin5.Template.Template.Device.Alias.Resources {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Strings {
@@ -39,7 +39,7 @@ namespace WHSPlugin5.Template.Template.Device.Alias.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WHSPlugin5.Template.Template.Device.Alias.Resources.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WHSPlugin6.Template.Template.Device.Alias.Resources.Strings", typeof(Strings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
